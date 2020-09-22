a7e4591f344086c3b27e76599fd719e774d1bcdf
flaviounicamp013 <flaviounicamp013@gmail.com>
2020-09-21 22:16:50 -0300

    Obtendo um Usuário por Id

---
+++ src/main/java/com/demo/mongodb/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.demo.mongodb.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	} 
+}
