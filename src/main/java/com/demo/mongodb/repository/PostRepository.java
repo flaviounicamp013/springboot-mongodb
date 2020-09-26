7868da5b8df6450803bc5363ffd0cd131766cdbf
flaviounicamp013 <flaviounicamp013@gmail.com>
2020-09-26 19:24:42 -0300

    Consulta Simples com @Query

--- src/main/java/com/demo/mongodb/repository/PostRepository.java
+++ src/main/java/com/demo/mongodb/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.demo.mongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.demo.mongodb.domain.Post;
@@ -10,6 +11,9 @@ import com.demo.mongodb.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);	
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
 
