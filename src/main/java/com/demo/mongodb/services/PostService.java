585ffe3097adfa565b5d82e024fbdd3bc8f05c6d
flaviounicamp013 <flaviounicamp013@gmail.com>
2020-09-26 19:07:39 -0300

    Consulta Simples com Query Methods

--- src/main/java/com/demo/mongodb/services/PostService.java
+++ src/main/java/com/demo/mongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.demo.mongodb.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,8 @@ public class PostService {
 		}
 		return user;
 	}
-
-
+	
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 }
